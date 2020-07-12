3a0beb6c17707fea8c86f56c9b1426834cb1c98c
kyshel <1069072177@qq.com>
2020-07-12 20:37:39 +0800

    docker OK!

--- README.md
+++ README.md
@@ -10,22 +10,11 @@ Code for spine-detect-competetion
 
 # next move
 
-# new (change route)
-1. make container
-1. container can migrate
-1. make a container that can truly run baseline
-1. migrate bigbox
-1. test
-1. submit
 
-
-## old
 1. pic rebuild , find relationship (core)
 1. extract features  (core)
 1. fit 
 1. verify
-1. wrap to cantainer
-1. server run
 
 
 
@@ -35,3 +24,9 @@ Code for spine-detect-competetion
 1. print img json info
 1. tag single pic in train51 
 1. tag multi pic
+1. make container
+1. container can migrate
+1. make a container that can truly run baseline
+1. migrate bigbox,  server run
+1. test
+1. submit
--- klib1.py
+++ klib1.py
@@ -1,5 +1,15 @@
+# klibX.py
+# X means version here
+# v1 indexListAccoed key
+# v2 save_obj and load_obj
+
+
+
+
 import json
 from pprint import pprint
+
+import pickle
 
 
 def test():
@@ -13,3 +23,18 @@ def indexListAccordKey(list,key):
         item.pop(key, None)
         indexedList[key_val] = item
     return indexedList
+
+
+def save_obj(obj_name,file_name):
+    with open(file_name, 'wb') as config_dictionary_file:
+        pickle.dump(obj_name, config_dictionary_file)
+
+        print(file_name + ' saved!')
+        return
+
+
+def load_obj(file_name):
+    with open(file_name, 'rb') as config_dictionary_file:
+        config_dictionary = pickle.load(config_dictionary_file)
+        print(file_name + ' loaded!')
+        return config_dictionary
